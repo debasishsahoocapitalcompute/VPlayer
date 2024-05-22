b05a2bca79570dfea25e1ae6bb5fd3a44a508d75
debasishsahoocapitalcompute <debasish@capitalcompute.com>
2024-05-23 01:30:52 +0530

    Added all functionalities

---
+++ app/UI/NewVideoDialog.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import Image from "next/image";
+import LogoColor from "../Assets/LogoColor.png"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import revalidateHomePage from "./RevalidateAction";
+
+async function postAllVideos(title: string, description: string, url: string) {
+    const res = await fetch('https://take-home-assessment-423502.uc.r.appspot.com/api/videos', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            user_id: "debasish_sahoo",
+            description: description,
+            video_url: url,
+            title: title
+        })
+    })
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return res.json()
+}
+
+export const NewVideoDialog = () => {
+    const [title, setTitle] = useState<string>("")
+    const [description, setDescription] = useState<string>("")
+    const [url, setUrl] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+    const handlePostVideo = async () => {
+        setLoading(true)
+        const data = await postAllVideos(title, description, url)
+        if (data["success"] === "POST /videos") {
+            revalidateHomePage()
+            setLoading(false)
+        } else {
+            setLoading(false)
+        }
+    }
+    return (
+        <div className="w-full flex justify-between items-center">
+            <div className="mb-5">
+            <Image
+                src={LogoColor}
+                alt="Picture of the author"
+                className="w-40 relative"
+            />
+            <p className="text-xs absolute top-[78px] left-[92px] text-slate-500">The first learning place for your kids</p>
+            </div>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant="default" className="w-32">Add new Video</Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Add New Video (by link)</DialogTitle>
+                        <DialogDescription>*every field is necessary</DialogDescription>
+                    </DialogHeader>
+                    <Input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} value={title} />
+                    <Input type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)} value={description} />
+                    <Input type="text" placeholder="URL" onChange={(e) => setUrl(e.target.value)} value={url} />
+                    <DialogFooter>
+                        <Button className="w-full" onClick={handlePostVideo}>{loading ? "Loading.." : "Upload"}</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Image from "next/image";
-import Video from 'next-video';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { NewVideoDialog } from "./UI/NewVideoDialog";
+import { VideoCard, videoObject } from './UI/VideoCard';
 
 async function getAllVideos() {
   const res = await fetch('https://take-home-assessment-423502.uc.r.appspot.com/api/videos?user_id=debasish_sahoo', {
@@ -19,30 +11,13 @@ async function getAllVideos() {
   return res.json()
 }
 
-type videoObject = {
-  created_at: string,
-  video_url: string,
-  user_id: string,
-  description: string,
-  title: string,
-  num_comments: number,
-  id: string
-}
-
 export default async function Home() {
   const { videos } = await getAllVideos()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {videos.map((video: videoObject) =>
-        <Card key={video.id} className="p-5 w-96">
-          <CardHeader>
-            <Video src={video.video_url} width={300} className="rounded-lg" />
-          </CardHeader>
-          <CardContent>
-            <CardTitle className="mt-5">{video.title}</CardTitle>
-            <CardDescription className="mt-4 mb-[-15px]">{video.description}</CardDescription>
-          </CardContent>
-        </Card>
+    <main className="flex min-h-screen flex-wrap items-left gap-5 p-10 w-full">
+      <NewVideoDialog />
+      {videos.reverse().map((video: videoObject) =>
+        <VideoCard videoObj={video} />
       )}
     </main>
   );
